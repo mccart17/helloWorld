880a2005b2a0af7c48302276396933377fcfa967
declanmccarthy <mccart17@terpmail.umd.edu>
2026-09-10 15:47:14 -0400

    Updated main page message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Declan McCarthy!'
+    return 'Hello World from Declan McCarthy! I am adding my first code change.'
 
 @app.route('/hello')
 def hello():
